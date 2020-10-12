45b63f2d8a1b3e082263b66b72a695e88c784a56
rixong <rixong@gmail.com>
2020-10-12 15:21:54 -0500

    fibanacci sum

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ for (let i = 1; i < 15; i += 1) {
 }
 
 console.log(result); */
+
+/* Fibonacci challenge
+By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+find the sum of the even-valued terms.
+ */
+
+function sumEvenFibs(n) {
+  let result = 0;
+  const arr = [1, 1];
+  let count = 2;
+  while (count < n) {
+    const total = arr[count - 1] + arr[count - 2];
+    if (total % 2 === 0) {
+      result += total;
+    }
+    arr.push(total);
+    count += 1;
+  }
+  return `The sum of the first '${n} Fibonacci numbers is ${result}`;
+}
+
+console.log(sumEvenFibs(10));
+
+// 1,1,2,3,5,8,13,21,34,55 => 2 + 8 + 34 = 44, n=10
