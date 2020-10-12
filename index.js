81e0b193b06e94f25a47a1ffd7f51b2269ffb90b
rixong <rixong@gmail.com>
2020-10-12 09:05:31 -0500

    finished multiples of 3 and 5

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-//Multiples of 3 and 5
+/*
+Multiples of 3 and 5
+Find the sum of all the multiples of 3 or 5 below 1000
+*/
 
-// for(let i=0)
+/* let result = 0;
+
+for (let i = 1; i < 15; i += 1) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    result += i;
+  }
+}
+
+console.log(result); */
