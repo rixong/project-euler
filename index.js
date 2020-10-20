1e3540f8826cfbc473f5f85472fb11c2aadfb89d
rixong <rixong@gmail.com>
2020-10-20 08:06:13 -0500

    fiished largest prime

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ By considering the terms in the Fibonacci sequence whose values do not exceed fo
 find the sum of the even-valued terms.
  */
 
-function sumEvenFibs() {
+/* function sumEvenFibs() {
   let result = 2;
   const arr = [1, 2];
   let count = 2;
@@ -36,10 +36,33 @@ function sumEvenFibs() {
 }
 
 console.log(sumEvenFibs());
-
+ */
 // 1,1,2,3,5,8,13,21,34,55 => 2 + 8 + 34 = 44, n=10
 
 /* Problem: Largest Prime Factor
 The prime factors of 13195 are 5, 7, 13 and 29. The largest prime factor in this case is 29.
 What is the largest prime factor of the number 600851475143 ?
 */
+
+/* 
+
+while result 
+
+*/
+
+function largestPrime(n) {
+  let result = n;
+  for (let i = 2; i < result; i += 1) {
+    while (result % i === 0 && result !== i) {
+      result /= i;
+    }
+  }
+  return result;
+}
+
+console.log(largestPrime(6857));
+
+// i =   2  2
+// res = 4  2
+
+
