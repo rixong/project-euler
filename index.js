dc3779f64c9730f6ca950e955f25c4266c72f94c
rixong <rixong@gmail.com>
2020-10-20 07:11:45 -0500

    sum even fibs

--- index.js
+++ index.js
@@ -38,3 +38,8 @@ function sumEvenFibs() {
 console.log(sumEvenFibs());
 
 // 1,1,2,3,5,8,13,21,34,55 => 2 + 8 + 34 = 44, n=10
+
+/* Problem: Largest Prime Factor
+The prime factors of 13195 are 5, 7, 13 and 29. The largest prime factor in this case is 29.
+What is the largest prime factor of the number 600851475143 ?
+*/
